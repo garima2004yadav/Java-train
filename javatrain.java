fe3d891b9622b82b7e39d8172f6941ce0caae251
garima2004yadav <115277218+garima2004yadav@users.noreply.github.com>
2023-10-17 22:05:00 +0530

    Add files via upload

---
+++ javatrain.java
@@ -0,0 +1,51 @@
+public class javatrain{
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.println(" ");
+        System.out.println("     *     ");
+        System.out.println("    * *    ");
+        System.out.println("   *   *   ");
+        System.out.print("  ");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.println("*");
+        System.out.println(" *       *  ");
+        System.out.println("*         * ");
+        System.out.println(" ");
+        System.out.println("*        *");
+        System.out.println("**      **");
+        System.out.println("* *    * *");
+        System.out.println("*  *  *  *");
+        System.out.println("*   **   *");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println(" ");
+        System.out.println("*        *");
+        System.out.println("**       *");
+        System.out.println("* *      *");
+        System.out.println("*  *     *");
+        System.out.println("*   *    *");
+        System.out.println("*    *   *");
+        System.out.println("*      * *");
+        System.out.println("*       **");
+    } 
+      /*   main();//userdefined overloaded main method
+    }
+    public static void main() {
+        System.out.println("this is demo overloaded");
+    }*/
+}
